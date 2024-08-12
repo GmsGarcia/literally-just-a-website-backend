1421ce255b402c75da2dd819032b2691d0a60c6a
Diogo Pires <diogo.pires2211@gmail.com>
2024-08-12 19:45:39 +0100

    improved logout responses

--- src/routes/auth/dashboard.js
+++ src/routes/auth/dashboard.js
@@ -4,8 +4,8 @@ export const dashboardRouter = express.Router();
 
 dashboardRouter.get('/', (req, res) => {
     if (!req.isAuthenticated()) {
-        res.redirect('/login')
+        res.status(401).json({ message: 'You are not logged in :(' })
     } else {
-        res.send('You are on your dashboard.')
+        res.status(200).json({ message: 'Welcome to your dashboard!' })
     }
 })
--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -5,8 +5,11 @@ export const logoutRouter = express.Router()
 logoutRouter.get('/', (req, res) => {
     req.logout(function (err) {
         if (err) {
-            return next(err);
+            console.error('Logout error:', err); 
+            return res.status(500).json({ message: 'An error occurred during logout.' });
+        } else {
+            console.log('Logout successful')
+            return res.status(200).json({ message: 'Successfully logged out.' })
         }
-        res.redirect('/login');
     })
 })
